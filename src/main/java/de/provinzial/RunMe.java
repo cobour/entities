4601a2d922c6fa88c84e3d1f1bdb3d1143f29656
Frank <cobour@web.de>
2021-02-23 08:07:45 +0100

    fixed java version and builder-call

--- src/main/java/de/provinzial/RunMe.java
+++ src/main/java/de/provinzial/RunMe.java
@@ -6,7 +6,7 @@ import de.provinzial.entities.CoolEntity.CoolEntityBuilder;
 public class RunMe {
 
 	public static void main(String[] args) {
-		CoolEntity entity = new CoolEntityBuilder().id(Long.valueOf(4711)).message("Hello world").build();
+		CoolEntity entity = CoolEntity.builder().id(Long.valueOf(4711)).message("Hello world").build();
 		System.out.println(entity.toString());
 	}
 }
